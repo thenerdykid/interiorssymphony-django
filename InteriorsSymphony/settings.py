cb91d7681050da3fa55698a93d1f1a505c256ddc
thenerdykid <jdg30001@outlook.fr>
2022-04-06 16:54:29 +0100

    Django Authentication

--- InteriorsSymphony/settings.py
+++ InteriorsSymphony/settings.py
@@ -69,7 +69,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'category.context_processors.menu_links',
-                'carts.context_processors.counter', 
+                'carts.context_processors.counter',
             ],
         },
     },
@@ -136,3 +136,15 @@ STATICFILES_DIRS = [
 # media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT= BASE_DIR /'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+#SMTP configuration
+EMAIL_HOST= 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'boutihkarim400@gmail.com'
+EMAIL_HOST_PASSWORD= 'Universrock1TCFRANCE'
+EMAIL_USE_TLS =  True
